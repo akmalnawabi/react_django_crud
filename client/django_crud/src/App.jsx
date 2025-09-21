92314228edaa4476ddf414e9a3c6aa42d4dfbb7d
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-21 16:06:48 +0330

    first

---
+++ client/django_crud/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Book Website</h1>
+
+      <div>
+        <input type="text" placeholder="book name" />
+        <input type="number" placeholder="Price" />
+        <button>Add Book</button>
+      </div>
+    </>
+  )
+}
+
+export default App
