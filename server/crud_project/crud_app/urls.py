884cfbfea531151f9a81759615213ea7a85021a0
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-24 11:08:41 +0330

    new

--- server/crud_project/crud_app/urls.py
+++ server/crud_project/crud_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_Books, create_Book
+from .views import get_Books, create_Book, book_detail
 
 urlpatterns = [
   path('books/', get_Books, name='get_Books'),
   path('books/create/', create_Book, name='create_Book'),
+  path('books/<int:pk>/', book_detail, name='book_detail'),
 ] 
